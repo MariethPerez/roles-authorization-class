fb8f24c259f39efb486c0228c09118b490c7cbd7
Arturo Puente <arturo@able.co>
2019-04-17 09:42:10 -0500

    Installed devise and pundit

---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+unless User.any?
+  User.create!(
+    name: "Jon Snow",
+    role: "member",
+    email: "jon@snow.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+
+  User.create!(
+    name: "Daenerys Targaryen",
+    role: "admin",
+    email: "dany@t.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+
+  User.create!(
+    name: "Tyrion Lannister",
+    role: "staff",
+    email: "tyrion@lannister.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+end
